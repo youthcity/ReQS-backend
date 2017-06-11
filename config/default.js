dd4f1612e30c5fb5ee21c5db010a3ec874e38a7e
tiancity <397731024@qq .com>
2017-06-11 22:30:36 +0800

    modify config

--- config/default.js
+++ config/default.js
@@ -5,7 +5,7 @@ module.exports = {
     key: 'treqs',
     maxAge: 2592000000
   },
-  mongodb: 'mongodb://localhost:27017/tblog',
+  mongodb: 'mongodb://tiancity:123@ds119682.mlab.com:19682/tblog',
   ACCESS_KEY: 'I35FAChy9k6QCpTJAXqjSXgw62Hwd-3JqNzBSHde',
   SECRET_KEY: 'fYWCfrFBPYpTgYZpFKDJnlsAxvNP7uXJ_GY-jzrh',
 };
